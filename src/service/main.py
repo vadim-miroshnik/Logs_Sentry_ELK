eaf0fee481712c947589c19094499dfd9c1f965c
Viatcheslav Pestov <pestov74@gmail.com>
2023-01-25 23:04:29 +0500

    update main

--- src/service/main.py
+++ src/service/main.py
@@ -5,7 +5,7 @@ from aiokafka import AIOKafkaProducer
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
-from api.v1 import watching
+from api.v1 import watching, reviews
 from core.config import settings
 from db import kafka
 
@@ -29,6 +29,8 @@ async def shutdown_event():
 
 
 app.include_router(watching.router, prefix="/api/v1/watching", tags=["films"])
+app.include_router(reviews.router, prefix="/api/v1/reviews", tags=["reviews"])
+
 
 if __name__ == "__main__":
     uvicorn.run(
