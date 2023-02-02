047203f3e53008351a0b002dcd1984734a32ab77
dovgopol.di <dmitrij.dowgopol@eltex.loc>
2023-02-02 23:42:04 +0700

    test fix mypy

--- src/service/auth/auth_handler.py
+++ src/service/auth/auth_handler.py
@@ -5,7 +5,7 @@ import jwt
 from core.config import settings
 
 
-def encode_jwt(user_id: str) -> Optional[Dict[str, Any]]:
+def encode_jwt(user_id: str) -> dict[str, str]:
     payload = {
         "user_id": user_id,
         "exp": time.time() + 600
@@ -17,7 +17,7 @@ def encode_jwt(user_id: str) -> Optional[Dict[str, Any]]:
     }
 
 
-def decode_jwt(token: str) -> dict:
+def decode_jwt(token: str) -> Optional[Dict[Any, Any]]:
     try:
         decoded_token = jwt.decode(token, settings.app.jwt_secret_key, algorithms=[settings.app.algorithm])
         return decoded_token if decoded_token["exp"] >= time.time() else None
