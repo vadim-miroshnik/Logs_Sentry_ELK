f2abb3e940979828b0b121b512b7d3700ddac5cc
dovgopol.di <dmitrij.dowgopol@eltex.loc>
2023-02-03 00:30:33 +0700

    fix mypy

--- src/service/storage/kafka.py
+++ src/service/storage/kafka.py
@@ -1,19 +1,6 @@
 from aiokafka import AIOKafkaProducer
 
-class Singleton(type):
-    _instance = None
-
-    def __call__(cls, *args, **kwargs):
-        if not cls._instance:
-            cls._instance = super().__call__(*args, **kwargs)
-        return cls._instance
-
-
-class Queues(dict, metaclass=Singleton):
-    pass
-
-
-queues = Queues()
+queues = dict()
 
 
 class KafkaService:
